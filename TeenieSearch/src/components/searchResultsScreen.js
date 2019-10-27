b9e3125ea867fb0442815fe9c334dffb134fb82f
Sam Ticknor <srticknor@gmail.com>
2019-10-27 17:00:55 -0400

    Started search screen

--- TeenieSearch/src/components/searchResultsScreen.js
+++ TeenieSearch/src/components/searchResultsScreen.js
@@ -4,9 +4,8 @@ import { search }from '../actions';
 
 import React, { Component } from 'react';
 import { StyleSheet, StatusBar, TouchableOpacity, ImageBackground, View, Text, TextInput } from 'react-native';
+import { Navigation } from 'react-native-navigation';
 import Icon from "react-native-vector-icons/Ionicons";
-
-import { SearchBar } from 'react-native-elements';
 
 // Style
 
@@ -15,10 +14,45 @@ const searchSuggestionPadding = 5;
 const styles = StyleSheet.create({
   section: {
     resizeMode: 'cover',
-    flex: .5,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'black'
+  },
+  subSection: {
+  	flex: 1,
+    flexGrow: 1,
+  	width: '100%',
+  	height: '100%',
+    justifyContent: 'space-between',
+    backgroundColor: '#eee',
+  },
+  sampleSearchContainer: {
+  	flex: 1,
+  	flexGrow: 1,
+    margin: searchSuggestionPadding,
+    backgroundColor: 'black',
+  },
+  sampleSearchImage: {
+  	width: '100%',
+  	height: '100%',
+  	alignItems: 'center',
+    justifyContent: 'center',
+  },
+  titleText: {
+  	fontSize: 40,
+  	color: 'white',
+  	marginBottom: 30,
+  },
+  sampleSearchText: {
+  	fontSize: 30,
+  	color: 'white',
+  },
+  contactButtonContainer: {
+  	width: '100%',
+  	height: '100%',
+  	backgroundColor: '#aaa',
+  	alignItems: 'center',
+  	justifyContent: 'center',
   },
   searchBar: {
   	backgroundColor: 'white',
@@ -35,7 +69,43 @@ const styles = StyleSheet.create({
 
 class searchResultsScreen extends Component {
 
+	
+
 	render() {
+		var suggestedSearchTerms = [
+			"Hill District",
+			"1950s",
+			"Jazz",
+			"Presidents",
+			"Civil Rights",
+			"Portraits",
+			"Teenie",
+			"Birthdays",
+			"Baseball",
+			"Crawford Grill",
+			"Puppies",
+			"Pittsburgh Courier"
+		]
+
+		var suggestedSearchView = suggestedSearchTerms.map((term, i) => {
+			return (
+				<TouchableOpacity 
+					style={styles.sampleSearchContainer}
+					onPress={() => this.props.actions.search(term)}
+				>
+					<ImageBackground 
+						style={styles.sampleSearchImage}
+						source={require('../assets/placeholder.jpg')}
+						imageStyle={{ opacity: .5, resizeMode: 'cover' }}
+					>
+						<Text style={styles.sampleSearchText}>
+							{ term }
+						</Text>
+					</ImageBackground>
+				</TouchableOpacity>
+			)
+		});
+
 		return(
 			<View style={{ flex : 1, width: '100%', height: '100%', backgroundColor: 'white' }}>
 				<StatusBar hidden></StatusBar>
@@ -53,10 +123,61 @@ class searchResultsScreen extends Component {
 				      style={{ flex: 1, fontSize: 20 }}
 				      // onChangeText={(text) => this.setState({text})}
 				      // onSubmitEditing={(event) => this.search(event.nativeEvent.text)}
-    				  // value={this.state.text}
+    				  value={this.props.term}
     				  keyboardAppearance={'light'}
 				    />
 				</View>
+
+				{this.props.results.length === 0 &&
+					<View style={styles.section}>
+						<Text> No results, try: </Text>
+						<View style={[styles.section, { flexDirection: 'column' }]}>
+							<View style={[styles.subSection, { flexDirection: 'row', paddingLeft: searchSuggestionPadding, paddingRight: searchSuggestionPadding, paddingTop: searchSuggestionPadding }]}>
+								{ suggestedSearchView[0] } 
+								{ suggestedSearchView[1] } 
+								{ suggestedSearchView[2] } 
+							</View>
+							<View style={[styles.subSection, { flexDirection: 'row', paddingLeft: searchSuggestionPadding, paddingRight: searchSuggestionPadding }]}>
+								{ suggestedSearchView[3] } 
+								{ suggestedSearchView[4] } 
+								{ suggestedSearchView[5] } 
+							</View>
+							<View style={[styles.subSection, { flexDirection: 'row', paddingLeft: searchSuggestionPadding, paddingRight: searchSuggestionPadding }]}>
+								{ suggestedSearchView[6] } 
+								{ suggestedSearchView[7] } 
+								{ suggestedSearchView[8] } 
+							</View>
+							<View style={[styles.subSection, { flexDirection: 'row', paddingLeft: searchSuggestionPadding, paddingRight: searchSuggestionPadding, paddingBottom: searchSuggestionPadding }]}>
+								{ suggestedSearchView[9] } 
+								{ suggestedSearchView[10] } 
+								{ suggestedSearchView[11] } 
+							</View>
+							<View style={[styles.subSection, { flexDirection: 'row' }]}>
+								<TouchableOpacity style={styles.contactButtonContainer} 
+									onPress={() => {
+										Navigation.push(this.props.componentId, {
+										  component: {
+										    name: 'aboutScreen',
+										    passProps: {},
+										    options: {
+										      topBar: {
+										        title: {
+										          text: "ABOUT"
+										        }
+										      }
+										    }
+										  }
+										});
+									}}
+								>
+									<Text style={styles.sampleSearchText}>
+										Reach Out
+									</Text>
+								</TouchableOpacity>
+							</View>
+						</View>
+					</View>
+				}
 
 
 
@@ -69,7 +190,8 @@ class searchResultsScreen extends Component {
 
 const mapStateToProps = state => {
   return {
-    input: state.search.input,
+    term: state.search.term,
+    results: state.search.results,
   };
 };
 

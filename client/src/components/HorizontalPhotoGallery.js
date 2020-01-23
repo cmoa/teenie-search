5358abdf43e6a5a765ceb7b2aa8b5fce19a7fa6d
Sam Ticknor <srticknor@gmail.com>
2020-01-23 10:59:44 -0500

    Prevent safari scrolling bug on horizontal scrollers

--- client/src/components/HorizontalPhotoGallery.js
+++ client/src/components/HorizontalPhotoGallery.js
@@ -23,10 +23,39 @@ const styles = {
 
 class HorizontalPhotoGallery extends Component {
 
+    constructor(props) {
+      super(props)
+      this.safariScrollerHorizontal = React.createRef();
+    }
+
+    componentDidMount() {
+      // Following code prevents safari from freezing up the scroll for 3 sec. scroll bug
+      var elem = this.safariScrollerHorizontal.current;
+      elem.addEventListener('touchstart', function(event){
+          this.allowLeft = (this.scrollLeft > 0);
+          this.allowRight = (this.scrollLeft < this.scrollWidth - this.clientWidth);
+          this.lastX = event.targetTouches[0].pageX;
+      });
+      elem.addEventListener('touchmove', function(event){
+          if (event.targetTouches.length > 0) {
+            var left = (event.targetTouches[0].pageX > this.lastX), 
+                right = !left;
+
+            this.lastX = event.targetTouches[0].pageX;
+
+            if ((left && this.allowLeft) || (right && this.allowRight)) {
+              event.stopPropagation();
+            } else {
+                event.preventDefault();
+            }         
+          }
+      });
+    }
+
     render() {
 
         return ( 
-          <div style={styles.photoSuggestions} className="smoothScroller"> 
+          <div style={styles.photoSuggestions} className="smoothScroller" ref={this.safariScrollerHorizontal}> 
                 { this.props.photos.map((photo, index) => {
                     return(
                       <img 
